1a5e12910a353ec5bd5aea06ae16205ca4b80347
Ayaon-Norhani <norhani.ayaon@gmail.com>
2024-08-22 11:30:06 +0800

    [Sitewide] changed labels

--- src/app/(customerFacing)/members/page.tsx
+++ src/app/(customerFacing)/members/page.tsx
@@ -15,7 +15,7 @@ const getProducts = cache(() => {
 const ProductsPage = (props: Props) => {
   return (
     <div className="">
-      <h2 className="text-3xl font-bold mb-5">All Members</h2>
+      <h2 className="text-3xl font-bold mb-5">Members</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-5xl">
         <Suspense
           fallback={
