aad77c14d7ff45e8e367904943ad6ed0dbb33af3
Ayaon-Norhani <norhani.ayaon@gmail.com>
2024-09-11 13:55:16 +0800

    [Navbar] made it transparent and make buttons thinner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "tailwindcss"
-import { fontFamily} from "tailwindcss/defaultTheme"
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -21,10 +21,10 @@ const config = {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
-        porscheNext: ['PorscheNext', 'sans-serif'],
+        porscheNext: ["PorscheNext", "sans-serif"],
       },
       aspectRatio: {
-        '3/2': '3 / 2',
+        "3/2": "3 / 2",
       },
       colors: {
         border: "hsl(var(--border))",
@@ -84,6 +84,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
